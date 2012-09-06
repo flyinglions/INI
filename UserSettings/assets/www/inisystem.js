6c481da7f510c99dbe9931eb3b776db299ea8f0f
Pieter <ppottas@vodamail.co.za>
2012-09-06 19:28:37 +0200

    added readFile support with callback

--- UserSettings/assets/www/inisystem.js
+++ UserSettings/assets/www/inisystem.js
@@ -11,21 +11,21 @@ var file_text="empty";
 
 // INI object:
 function Ini(contents){
-//alert('ini created');
-        this.sections = new Array();
+    //alert('ini created');
+    this.sections = new Array();
         
-        this.prse = function (content,inis) {
-parse(content,inis);
-}
+    this.prse = function (content,inis) {
+        parse(content,inis);
+    }
 
-// The parser function - takes text and transforms it into the structure set out by the INI class
+    // The parser function - takes text and transforms it into the structure set out by the INI class
 
-        parse = function (content,inis) {
-            var i, ln, m, key, val, prop = '',
-            lines = contents.split(/\r\n|\r|\n/);
-//alert("parsing");
+    parse = function (content,inis) {
+        var i, ln, m, key, val, prop = '',
+        lines = contents.split(/\r\n|\r|\n/);
+        //alert("parsing");
         
-var index =-1;
+        var index =-1;
 
         for (i=0; i<lines.length; i++) {
            
@@ -53,11 +53,11 @@ var index =-1;
 
 
                         
-                        inis.push(new Section(m1));
-                        index++;
-                        //alert(inis[0].name);
+                    inis.push(new Section(m1));
+                    index++;
+                //alert(inis[0].name);
 
-                    //sect = up_sect.section(m1, m3 || null);
+                //sect = up_sect.section(m1, m3 || null);
                 });
                 continue;
             }
@@ -81,10 +81,10 @@ var index =-1;
                     if (val.charAt(0) === '"' && val.charAt(val.length-1) === '"') {
                         val = val.substr(1, val.length-2);
                         val = val.replace(/\\\\/g, '\\')
-                                 .replace(/\"/g, '"')
-                                 .replace(/\\n/g, '\n')
-                                 .replace(/\\t/g, '\t')
-                                 .replace(/\\b/g, '\b');
+                        .replace(/\"/g, '"')
+                        .replace(/\\n/g, '\n')
+                        .replace(/\\t/g, '\t')
+                        .replace(/\\b/g, '\b');
                     // Check boolean
                     } else if (/^yes|on|true$/i.test(val)) {
                         val = true;
@@ -101,49 +101,49 @@ var index =-1;
                     val = true;
                 }
 
-inis[index].add(new Property(key,val));
-                //sect.property(key, val);
+                inis[index].add(new Property(key,val));
+            //sect.property(key, val);
             }
 
             prop = '';
         }
-        }
+    }
         
-        parse(contents,this.sections);
+    parse(contents,this.sections);
         
 
         
-    }
-    
-    function Section(name) {
-         
-this.name = name;
-this.items = new Array();
-           
-this.add = function (prop) {
-this.items.push(prop);
-                    
 }
-            
-    }
     
-    function Property(name,val) {
-this.name = name;
-this.val = val;
-            
+function Section(name) {
+         
+    this.name = name;
+    this.items = new Array();
+           
+    this.add = function (prop) {
+        this.items.push(prop);
+                    
     }
+            
+}
+    
+function Property(name,val) {
+    this.name = name;
+    this.val = val;
+            
+}
     
   
- // Transforms INI object into string using theini variable declared at the top
+// Transforms INI object into string using theini variable declared at the top
 function toStr() {
-	var s="";
-	for (var k =0; k<theini.sections.length; k++ ) {
-		s+="["+theini.sections[k].name+"]\n";
-		for (var p =0; p<theini.sections[k].items.length; p++ ) {
-			s+=theini.sections[k].items[p].name+"="+theini.sections[k].items[p].val+"\n";
-		}
-	}
-	return s;
+    var s="";
+    for (var k =0; k<theini.sections.length; k++ ) {
+        s+="["+theini.sections[k].name+"]\n";
+        for (var p =0; p<theini.sections[k].items.length; p++ ) {
+            s+=theini.sections[k].items[p].name+"="+theini.sections[k].items[p].val+"\n";
+        }
+    }
+    return s;
 }
 
 
@@ -155,24 +155,26 @@ function setFileName(fname) {
 
 function fileSystemInit() {
 
-//i made the filesize 100kb....?
-  window.requestFileSystem(1, 1024 * 100, gotFSSuccess, failed);
-	//failed - function to call when failed
+    //i made the filesize 100kb....?
+    window.requestFileSystem(1, 1024 * 100, gotFSSuccess, failed);
+//failed - function to call when failed
 }
 
 
 function gotFSSuccess(filesystem) {
-   // Got the filesystem 
+    // Got the filesystem 
     FS = filesystem;   
-    FS.root.getFile(filename,{create : true},gotFileEntry,failed);
+    FS.root.getFile(filename,{
+        create : true
+    },gotFileEntry,failed);
 }
 
 function gotFileEntry(theFile) {
-// alert("onGetFileSuccess: " + theFile.name);
+    // alert("onGetFileSuccess: " + theFile.name);
 
     file_entry = theFile;
-readFromFile();
-    //thefileentry.file(gotFile,failed);
+    readFromFile();
+//thefileentry.file(gotFile,failed);
 }
 
 /*Reading the file here*/
@@ -182,28 +184,28 @@ function readFromFile() {
 }
 
 function fileReaderSuccess(file) {
-//alert("get reading file");
+    //alert("get reading file");
     var reader = new FileReader();
     reader.onloadend = function(e) {
     
-     file_text = e.target.result;
-     alert("read:"+file_text);
+        file_text = e.target.result;
+        alert("read:"+file_text);
 
-if (file_text.length==0) {
+        if (file_text.length==0) {
 
-file_text="[author]\nname=Pieter\n[user]";
-  }
- // alert("file_text: "+file_text);
-  theini = new Ini(file_text);
+            file_text="[author]\nname=Pieter\n[user]";
+        }
+        // alert("file_text: "+file_text);
+        theini = new Ini(file_text);
 
     };
     reader.onloaderror = function(e) {
-    failed;
-  };
+        failed;
+    };
   
-  reader.readAsText(file);
+    reader.readAsText(file);
   
-  /*if (file_text.length===0)
+/*if (file_text.length===0)
 ini = new Ini('[user]');
 else
 ini = new Ini(file_text);*/
@@ -214,18 +216,20 @@ ini = new Ini(file_text);*/
 
 
 function writeToFile() {
-FS.root.getFile(filename,{create : true},gotFileEntryforWriter,failed);
+    FS.root.getFile(filename,{
+        create : true
+    },gotFileEntryforWriter,failed);
     
 }
 
 function gotFileEntryforWriter(theFile) {
-theFile.createWriter(fileWriterSuccess, failed);
+    theFile.createWriter(fileWriterSuccess, failed);
 }
 
 function fileWriterSuccess(writer) {
     writer.onwrite = function(e) {
-//alert("Write Completed");
-  };
+    //alert("Write Completed");
+    };
     writer.write(toStr());//'[author]\nname=Pieter\n[user]');
     
 }
@@ -271,21 +275,21 @@ function failed() {
     
     
     
-    //var ini;
+//var ini;
     
     
     
-   
+
     
     
 function startINI() {
-  fileSystemInit();
-alert("init success");
-  //filename = "settings.ini";
+    fileSystemInit();
+    alert("init success");
+//filename = "settings.ini";
    
   
-    //get text from phonegap-api
-  /* ini = new Ini([
+//get text from phonegap-api
+/* ini = new Ini([
 '[user]',
 'foo = ho iszit',
 'baz = "tttt"',
@@ -307,11 +311,12 @@ function stopINI() {
     //write to file
     //file_text =
     writeToFile();
-    //delete ini;
+//delete ini;
 }
 
 function INIset(isection,ikey,ivalue) {
-/*if (ini==null)
+    
+    /*if (ini==null)
 ini = new INI('[user]');*/
     var sectionset=false;
     var keyset=false;
@@ -326,7 +331,7 @@ ini = new INI('[user]');*/
                 
             }
             if (!keyset) {
-                    theini.sections[k].items.push(new Property(ikey,ivalue));
+                theini.sections[k].items.push(new Property(ikey,ivalue));
             }
             
             sectionset = true;
@@ -337,21 +342,21 @@ ini = new INI('[user]');*/
             
     }
     if (!sectionset) {
-            theini.sections.push(new Section(isection));
-            theini.sections[ini.sections.length-1].items.push(new Property(ikey,ivalue));
-            return true;
+        theini.sections.push(new Section(isection));
+        theini.sections[ini.sections.length-1].items.push(new Property(ikey,ivalue));
+        return true;
     }
     
 }
 function INIget(section,key) {
     for (var k =0; k<theini.sections.length; k++ ) {
         if (theini.sections[k].name == section)
-        for (var p =0; p<theini.sections[k].items.length; p++ ) {
-            if (theini.sections[k].items[p].name==key) {
+            for (var p =0; p<theini.sections[k].items.length; p++ ) {
+                if (theini.sections[k].items[p].name==key) {
                 
-                return theini.sections[k].items[p].val;
+                    return theini.sections[k].items[p].val;
+                }
             }
-        }
         
     }
     
@@ -374,12 +379,12 @@ var regular_FS;
 //var regular_file_entry;
 
 function regular_fileSystemInit() {
-	window.requestFileSystem(1,0, regular_gotFSSuccess, failed);
-	//failed - function to call when failed
+    window.requestFileSystem(1,0, regular_gotFSSuccess, failed);
+//failed - function to call when failed
 }
 function regular_gotFSSuccess(filesystem) {
-   // Got the filesystem 
-	console.log("got filesystem");
+    // Got the filesystem 
+    console.log("got filesystem");
     regular_FS = filesystem;   
   
 }
@@ -389,112 +394,153 @@ function regular_gotFSSuccess(filesystem) {
 /*=============================*/
 /*READING FROM THE FILE THAT IS LOADED*/
 /*=============================*/
+var file_to_read="";
+var readcallback;
+function failed_read(error) {
+    console.log("reading error: "+error);
+}
 
-function failed_read(error) { console.log("reading error: "+error); }
-
+function readFile(fname,call) {
+    readcallback = call;
+    file_to_read=fname;
+    window.requestFileSystem(1,0, rgotFSSuccess, failed_read);	
+}
+function rgotFSSuccess(filesystem) {
+    // Got the filesystem 
+    console.log("got filesystem");
+    regular_readFromFile(filesystem,file_to_read);    
+}
 //function to initialize the reading of a file specified by the {regular_file_entry}
-function regular_readFromFile(fname) {
-	regular_FS.root.getFile(fname,{create : false},read_gotFileEntry,failed_read);  
-    
+function regular_readFromFile(filesystem,fname) {
+    filesystem.root.getFile(fname,{
+        create : false
+    },read_gotFileEntry,failed_read);     
 }
 
 function read_gotFileEntry(theFile) {	//regular_file_entry = theFile;	
-	theFile.file(read_fileReaderSuccess,failed_read);
+    theFile.file(read_fileReaderSuccess,failed_read);
+    
 }
 
 //got file - now load the content:
 function read_fileReaderSuccess(file) {
-	var reader = new FileReader();
-	reader.onloadend = function(e) {    
-		file_content = e.target.result;
-		console.log("file_content"+file_content);
+    var reader = new FileReader();
+    reader.onloadend = function(e) {    
+        var text = e.target.result;
+        console.log("file_content"+text);
 
-		if (file_text.length==0) {
-			console.log("file empty!");
-		} 
-	};
-	reader.onloaderror = function(e) {
-		console.log("could not load file contents");
-	};
+        if (text.length==0) {
+            console.log("file empty!");
+        } 
+        readcallback(text);
+    };
+    reader.onloaderror = function(e) {
+        console.log("could not load file contents");
+    };
   
-	reader.readAsText(file);  
+    reader.readAsText(file);  
 }
 
 /*===========================*/
 /*WRITING TO THE FILE THAT IS LOADED*/ /*Using regular_FS (for now)
 /*===========================*/
-
-function write_failed(error) { console.log("writing error: " + error); }
+/*remember to add regular_fileSystemInit when beginning */
+function write_failed(error) {
+    console.log("writing error: " + error);
+}
 
 function regular_writeToFile(fname) {
-	regular_FS.root.getFile(fname,{create : true},write_gotFileEntry,write_failed);    
+    regular_FS.root.getFile(fname,{
+        create : true
+    },write_gotFileEntry,write_failed);    
 }
 
 function write_gotFileEntry(theFile) {
-theFile.createWriter(write_fileWriterSuccess, write_failed);
+    theFile.createWriter(write_fileWriterSuccess, write_failed);
 }
 
 function write_fileWriterSuccess(writer) {
-	writer.onwrite = function(e) {
-		console.log("Writing done!"+file_content);
-	};	
-	writer.write(file_content);
+    writer.onwrite = function(e) {
+        console.log("Writing done!"+file_content);
+    };	
+    writer.write(file_content);
 }
 /*=========================*/
 /* DELETING OF FILE */
 /*=========================*/
 
-function fail_delete(error) { console.log("file could not be deleted: "+error); }
-function success_delete(entry) { console.log("file deleted successfully"); }
-
-function performdeletion(fname) {
-	regular_FS.root.getFile(fname,{create : false},entryfordelete,fail_delete);    
+function fail_delete(error) {
+    console.log("file could not be deleted: "+error);
+}
+function success_delete(entry) {
+    console.log("file deleted successfully");
 }
 
+var file_to_delete="";
+function deleteFile(thefile) {
+    file_to_delete = thefile;
+    window.requestFileSystem(1,0, dgotFSSuccess, fail_delete);
+//failed - function to call when failed
+}
+function dgotFSSuccess(filesystem) {
+    // Got the filesystem 
+    console.log("got filesystem");
+    performdeletion(filesystem,file_to_delete)  ;  
+}
+
+function performdeletion(filesystem,fname) {
+    filesystem.root.getFile(fname,{
+        create : false
+    },entryfordelete,fail_delete);    
+}
 //removing the file
-function entryfordelete(theFile) {	theFile.remove(success_delete,fail_delete); }
+function entryfordelete(theFile) {
+    theFile.remove(success_delete,fail_delete);
+}
 
 /*=========================================================*/
 /* READ DIRECTORY AND SAVE CONTENT TO {dir_entries}*/
 /*========================================================*/
-function file_error(error) { console.log("error reading dir"+error); }
+function file_error(error) {
+    console.log("error reading dir"+error);
+}
 function getDirectoryEntries() { 
-  window.requestFileSystem(1, 0, onGetFileSystemSuccess, file_error);
+    window.requestFileSystem(1, 0, onGetFileSystemSuccess, file_error);
 }
 
 function onGetFileSystemSuccess(fs) {  
-  //theFileSystem = fs;
-  var dr = fs.root.createReader();
-  // Get a list of all the entries in the directory
-  dr.readEntries(onDirReaderSuccess, file_error);
+    //theFileSystem = fs;
+    var dr = fs.root.createReader();
+    // Get a list of all the entries in the directory
+    dr.readEntries(onDirReaderSuccess, file_error);
 }
 
 var dir_entries_arr = new Array();
 
 function onDirReaderSuccess(dirEntries) {
-  console.log("#dir entries (" + dirEntries.length + ")");
+    console.log("#dir entries (" + dirEntries.length + ")");
   
-  var num_file_entries = 0;
-  var i,  len;
-  len = dirEntries.length;
-  if(len > 0) {
+    var num_file_entries = 0;
+    var i,  len;
+    len = dirEntries.length;
+    if(len > 0) {
     
-    for( i = 0; i < len; i++) {
-      if(dirEntries[i].isDirectory == true) {
-	      console.log("dir: "+dirEntries[i].name);
+        for( i = 0; i < len; i++) {
+            if(dirEntries[i].isDirectory == true) {
+                console.log("dir: "+dirEntries[i].name);
         
-      } else {
-	      console.log("file: "+dirEntries[i].name);
-        dir_entries_arr[num_file_entries] = dirEntries[i].name;
-	      num_file_entries =num_file_entries+1;
-      }
-    }
+            } else {
+                console.log("file: "+dirEntries[i].name);
+                dir_entries_arr[num_file_entries] = dirEntries[i].name;
+                num_file_entries =num_file_entries+1;
+            }
+        }
     
     
-  } else {
-    console.log("No entries found");
+    } else {
+        console.log("No entries found");
     
-  } 
+    } 
   
 }
 
@@ -503,22 +549,23 @@ function onDirReaderSuccess(dirEntries) {
 /*=========================*/
 
 function initFileSystem() {	
-	console.log("initing the filesystem..");
-	regular_fileSystemInit();
+    console.log("initing the filesystem..");
+    regular_fileSystemInit();
 }
 
 //function to read a file and store the contents in the file_text variable
+/*
 function readFile(fname) {
-	regular_readFromFile(fname);    
-}
+regular_readFromFile(fname);    
+}*/
 
 function writeFile(fname) {
-	regular_writeToFile(fname);
+    regular_writeToFile(fname);
 }
-
+/*
 function deleteFile(fname) {
-	performdeletion(fname);	
-}
+performdeletion(fname);	
+}*/
 
 //for getting a list of all files in a directory (root - for now)
 //getDirectoryEntries
